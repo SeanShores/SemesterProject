5d6663f3b8a7addeaadd60db0eedd407ba7ba7bf
Dylan Aviles <Avild@farmingdale.edu>
2019-11-17 19:25:00 -0500

    Main and controller classes created, along with basic GUI layout.

---
+++ src/CardsController.java
@@ -0,0 +1,38 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class CardsController {
+
+    @FXML
+    private Button btnSolution;
+
+    @FXML
+    private TextField txtTop;
+
+    @FXML
+    private Button btnRefresh;
+
+    @FXML
+    private TextField txtBottom;
+
+    @FXML
+    private Button btnVerfiy;
+
+    @FXML
+    void findSolution(ActionEvent event) {
+
+    }
+
+    @FXML
+    void refresh(ActionEvent event) {
+
+    }
+
+    @FXML
+    void verify(ActionEvent event) {
+
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage pStage) throws Exception {
+		Parent p = FXMLLoader.load(getClass().getResource("CardsLayout.fxml"));
+		Scene cards = new Scene(p);
+		
+		pStage.setTitle("Playing Cards 24");
+		pStage.setScene(cards);
+		pStage.show();
+	}
+
+}
