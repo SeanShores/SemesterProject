f22abedd18fd92afbb6d60a5d987e1da850793a0
Group 4 <davil544@gmail.com>
2019-12-04 18:03:14 -0500

    Got Find Solution button working and most of the verification
    Verify still does not work properly when checking numbers used in the
    expression with the card numbers.

--- src/CardsController.java
+++ src/CardsController.java
@@ -1,5 +1,6 @@
-import java.util.HashSet;
-import java.util.Set;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -11,6 +12,8 @@ import javafx.scene.image.ImageView;
 
 public class CardsController {
 
+	static String cardNum[] = new String[4];
+	
 	@FXML
 	private Pane pnCards;
 
@@ -35,6 +38,39 @@ public class CardsController {
 	@FXML
 	void findSolution(ActionEvent event) {
 		//Use the card numbers to find a solution that adds to 24
+		ScriptEngineManager sem = new ScriptEngineManager();
+	    ScriptEngine engine = sem.getEngineByName("javascript");
+
+	    String solution = null;
+	    for (int i = 0; i < 4; i++) {
+	    	System.out.println("Card " + i + ": " + cardNum[i]);
+	    }
+	    
+	    char[] operations = new char[] { '+', '-', '*', '/' };
+	    
+	    for (int i = 0; i < 4; i++) {
+	        for (int j = 0; j < 4; j++) {
+	            for (int k = 0; k < 4; k++) {
+	            	
+	                try {
+	                    String exp = "" + cardNum[0] + operations[i] + cardNum[1] + operations[j]
+	                            + cardNum[2] + operations[k] + cardNum[3];
+	                    String res = engine.eval(exp).toString();
+	                    if (Double.valueOf(res).intValue() == 24) {
+	                        solution = exp;
+	                        txtTop.setText(exp);
+	                        System.out.println("Solution: " + exp);
+	                    }
+	                } catch (ScriptException e) {
+	                    System.out.println("Error");
+	                }
+	            }
+	        }
+	    }
+	    if (solution == null) {
+	    	txtTop.setText("No solution");
+	    	System.out.println("No Solution");
+	    }
 	}
 
 	@FXML
@@ -49,6 +85,7 @@ public class CardsController {
 			//If statement for card suite
 			int card = (int) (Math.random() * 13),
 					suiteNum = (int) (Math.random() * 4);
+			
 
 			while (suiteNum == 0)
 				suiteNum = (int) (Math.random() * 4);
@@ -87,6 +124,8 @@ public class CardsController {
 				cardType = card + "";
 			}
 			
+			cardNum[count] = card + "";
+			System.out.println("Card number "+ count + " = " + cardNum[count]);
 
 			String fileName = "file:Cards/" + cardType + "_of_" + suite + ".png";
 			if (!(usedCards[card * suiteNum])) {
@@ -107,11 +146,52 @@ public class CardsController {
 	}
 
 	@FXML
-	void verify(ActionEvent event) {
-		char ch = '?';
-		if (ch == '(' || ch == ')' || ch == '/' || ch == '+' || ch == '-' || ch == '*') {
-			//see if expression adds up to 24?
+	int verify(ActionEvent event) throws ScriptException {
+		ScriptEngineManager mgr = new ScriptEngineManager();
+        ScriptEngine js = mgr.getEngineByName("JavaScript");
+        
+        //See if spaces can be added in between the operators to recognize
+        //valid numbers in the expression?
+	    String expression = "" + js.eval( txtBottom.getText() );
+	    boolean validOperator = false;
+        
+        if (expression.contains("+") || expression.contains("-") ||
+        		expression.contains("*") || expression.contains("/")) {
+        	validOperator = true;
+        }
+        
+        //For some reason the cards are showing up invalid even
+        //though they are valid.  WHY?????
+        for (int i = 0; i < 4; i++) {
+        	System.out.print("Card " + i + " valid? ");
+        	if (expression.contains(cardNum[i] + "")) {
+        		System.out.println(cardNum[i] + ": True");
+        	}
+        	else {
+        		System.out.println(cardNum[i] + ": False");
+        	}
+        } 
+        
+	    
+	    if ( !expression.contains(cardNum[0] + "") || !expression.contains(cardNum[1] + "") ||
+	    		!expression.contains(cardNum[2] + "") || !expression.contains(cardNum[3] + "") ||
+	    		!expression.contains("+") || !validOperator) {
+	    	System.out.println("Not right dude");
+	    	txtTop.setText("Use card numbers only!");
+	    	return 1;
+	    	
+	    }
+	    
+	    int exp = Math.round( Integer.parseInt(expression) );
+	    System.out.println(exp);
+	    
+	    if (exp == 24) {
+			txtTop.setText("Valid!");
 		}
+		else {
+			txtTop.setText("Invalid!");
+		}
+	    return 0;
 	}
 
 
